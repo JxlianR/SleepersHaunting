ec7aa34b907c41f187e30bbdef3853f791dcf9b1
JxlianR <j.reiter022002@gmail.com>
2023-11-03 09:46:44 +0000

    Sleeper's Haunting Project

---
+++ UnrealEngine-5.2/SleepersHaunting/Source/SleepersHaunting/SleepersHaunting.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SleepersHaunting.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SleepersHaunting, "SleepersHaunting" );
