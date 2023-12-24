3e8274c842f927e79e8feb0a1b63e574405dbe56
NekrosXPrime <83087418+NekrosXPrime@users.noreply.github.com>
2023-12-24 19:47:08 +0000

    Revert "Revert "Added the new stuff""
    This reverts commit b25e549baca7e9663d7293704cfc206f4689b398.

---
+++ UnrealEngine-5.2/SleepersHaunting/Source/SleepersHaunting/MyGameState.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "GameFramework/Actor.h"
+
+//Place Other C++ classes here
+#include "Roomba.h" //Julian needds to add code here.
+#include "PlayerCharacter.h" //Also make the players here stop moving
+
+#include "MyGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLEEPERSHAUNTING_API AMyGameState : public AGameState
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	AMyGameState();
+
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
+ 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Replicated, Category = "Conditions")
+		bool losingCondition;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Replicated, Category = "Conditions")
+		bool winningCondition;
+
+	//Classes
+	class AA_FatherGarage* FatherGarageInstance;
+	class ATheTwins* TheTwinsInstance_1;
+	class ATheTwins* TheTwinsInstance_2;
+	//ARoomba* RoombaInstance; //Someone do this
+	class AClockTimer* ClockTimerInstance;
+	class AGroupAttack* GroupAttackInstance;
+	class APowerSystem* PowerSystemInstance;
+
+
+	//APlayerCharacter* PlayerCharacterInstance; //Someone do this
+
+	UFUNCTION()
+		void SetLoseCondition(int nKiller);
+
+	
+
+	UFUNCTION()
+		void WinConditionEvent();
+private:
+	UFUNCTION()
+		void GetAllClassesInstances();
+	UFUNCTION()
+		void StopAllEventsTimers();
+	//UFUNCTION()
+		//void LoseConditionEvent();
+
+};
