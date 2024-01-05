83a0de1e391808e37855c4752c24cd0508b04c82
NekrosXPrime <83087418+NekrosXPrime@users.noreply.github.com>
2024-01-05 23:22:01 +0000

    Lobby

--- UnrealEngine-5.2/SleepersHaunting/Source/SleepersHaunting/SHLobby.cpp
+++ UnrealEngine-5.2/SleepersHaunting/Source/SleepersHaunting/SHLobby.cpp
@@ -2,73 +2,96 @@
 
 
 #include "SHLobby.h"
-#include "Kismet/GameplayStatics.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Components/CapsuleComponent.h"
 #include "Net/UnrealNetwork.h"
 
 // Sets default values
 ASHLobby::ASHLobby()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	// Set default values
 	CountPlayer = 0;
 	TimerDuration = 10.0f; // Set the duration as needed
 
-	// Create the lobby box component and set it as the root component
-	LobbyBox = CreateDefaultSubobject<UBoxComponent>(TEXT("LobbyBox"));
-	RootComponent = LobbyBox;
+	// Create the lobby mesh component and set it as the root component
+	LobbyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LobbyMesh"));
+	RootComponent = LobbyMesh;
+
+	// Set the cube mesh for the LobbyMesh component
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeMeshAsset(TEXT("StaticMesh'/Game/Path/To/Your/CubeMesh.CubeMesh'"));
+	if (CubeMeshAsset.Succeeded())
+	{
+		LobbyMesh->SetStaticMesh(CubeMeshAsset.Object);
+	}
 
 	// Bind functions to handle overlap events
-	LobbyBox->OnComponentBeginOverlap.AddDynamic(this, &ASHLobby::OnLobbyEnter);
-	LobbyBox->OnComponentEndOverlap.AddDynamic(this, &ASHLobby::OnLobbyExit);
+	LobbyMesh->OnComponentBeginOverlap.AddDynamic(this, &ASHLobby::OnLobbyEnter);
+	LobbyMesh->OnComponentEndOverlap.AddDynamic(this, &ASHLobby::OnLobbyExit);
 }
 
 // Called when the game starts or when spawned
 void ASHLobby::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void ASHLobby::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 void ASHLobby::OnLobbyEnter(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	// Check if the overlapped actor is a player
+	// Check if the overlapped actor is a player character
 	ACharacter* PlayerCharacter = Cast<ACharacter>(OtherActor);
 	if (PlayerCharacter)
 	{
-		// Increment player count
-		CountPlayer++;
-
-		// Start the lobby timer if it's the first player
-		if (CountPlayer == 2)
+		// Check if the overlapped component is a capsule component
+		UCapsuleComponent* CapsuleComponent = Cast<UCapsuleComponent>(OtherComp);
+		if (CapsuleComponent)
 		{
-			StartLobbyTimer();
+			// Increment player count
+			CountPlayer++;
+
+			// Display debug message
+			FString DebugMessage = FString::Printf(TEXT("Player entered the lobby! Players inside: %d"), CountPlayer);
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, DebugMessage);
+
+			// Start the lobby timer if it's the first player
+			if (CountPlayer == 2)
+			{
+				StartLobbyTimer();
+			}
 		}
 	}
 }
 
 void ASHLobby::OnLobbyExit(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	// Check if the overlapped actor is a player
+	// Check if the overlapped actor is a player character
 	ACharacter* PlayerCharacter = Cast<ACharacter>(OtherActor);
 	if (PlayerCharacter)
 	{
-		// Decrement player count
-		CountPlayer--;
-
-		// Stop the lobby timer if there are less than 2 players
-		if (CountPlayer < 2)
+		// Check if the overlapped component is a capsule component (commonly used for player bodies)
+		UCapsuleComponent* CapsuleComponent = Cast<UCapsuleComponent>(OtherComp);
+		if (CapsuleComponent)
 		{
-			StopLobbyTimer();
+			// Decrement player count
+			CountPlayer--;
+
+			// Display debug message
+			FString DebugMessage = FString::Printf(TEXT("Player exited the lobby! Players inside: %d"), CountPlayer);
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, DebugMessage);
+
+			// Stop the lobby timer if there are less than 2 players
+			if (CountPlayer < 2)
+			{
+				StopLobbyTimer();
+			}
 		}
 	}
 }
@@ -82,7 +105,8 @@ void ASHLobby::StartLobbyTimer()
 void ASHLobby::StopLobbyTimer()
 {
 	// Stop the timer
-	if (GetWorldTimerManager().IsTimerActive(LobbyTimerHandle)) {
+	if (GetWorldTimerManager().IsTimerActive(LobbyTimerHandle))
+	{
 		GetWorldTimerManager().ClearTimer(LobbyTimerHandle);
 	}
 }
@@ -96,6 +120,5 @@ void ASHLobby::OpenNextLevel()
 void ASHLobby::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-
 	DOREPLIFETIME(ASHLobby, CountPlayer);
 }
--- UnrealEngine-5.2/SleepersHaunting/Source/SleepersHaunting/SHLobby.h
+++ UnrealEngine-5.2/SleepersHaunting/Source/SleepersHaunting/SHLobby.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
 #include "Components/BoxComponent.h"
+#include "Components/CapsuleComponent.h" // Include for UCapsuleComponent
 #include "TimerManager.h"
 #include "SHLobby.generated.h"
 
@@ -12,43 +14,45 @@ UCLASS()
 class SLEEPERSHAUNTING_API ASHLobby : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	ASHLobby();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
+public:
 	virtual void Tick(float DeltaTime) override;
 
 	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay", Replicated)
+		int32 CountPlayer;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay")
+		float TimerDuration;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+		UStaticMeshComponent* LobbyMesh;
 private:
 	FTimerHandle LobbyTimerHandle;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay", meta = (AllowPrivateAccess = "true", Replicated))
-		int32 CountPlayer;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gameplay", meta = (AllowPrivateAccess = "true"))
-		float TimerDuration;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-		UBoxComponent* LobbyBox;
+
 
 	UFUNCTION()
 		void OnLobbyEnter(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UFUNCTION()
 		void OnLobbyExit(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 	UFUNCTION()
 		void StartLobbyTimer();
+
 	UFUNCTION()
 		void StopLobbyTimer();
+
 	UFUNCTION()
 		void OpenNextLevel();
 };
